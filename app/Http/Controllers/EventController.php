bc7b55991f5759813cf45a3e833a1c6a7381d799
Vitor Mariotto <vitormariotto03@gmail.com>
2024-08-20 14:35:26 -0300

    feat: unsure message success create event

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -28,6 +28,6 @@ class EventController extends Controller
 
         $event->save();
 
-        return redirect('/');
+        return redirect('/')->with('msg', 'Evento criado com sucesso!');
     }
 }
