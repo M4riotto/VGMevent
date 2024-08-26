8762f5777e399fea66c34fe492dd33effa84a2c2
Vitor Mariotto <vitormariotto03@gmail.com>
2024-08-26 12:18:41 -0300

    feat: install jetstream and livewire for LOGIN and LOGOUT

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,13 @@ Route::post('/events',  [EventController::class, 'store']);
 Route::get('/contact', function () {
     return view('contact');
 });
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
